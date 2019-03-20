defb15b1ec319d698cb447dfb2f0766a153f4bf5
mjag02 <mjag02@gmail.com>
2019-03-19 20:49:14 -0400

    1. Added a new Visual C# Console Application to your repository called Lab01. 2. Modify the Console Application, so that it displays Hello World from [Your Name] to the Command Prompt window. 3. Create two string variables in your program to hold 1) The greeting (Hello World) and 2) your name.  Output Hello World from [Your Name] using a single Console.WriteLine() statement and the variables

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello World using Console.WriteLine()statement
+            Console.WriteLine("Hello World from Marilyn Green");
+           
+            //declaring 2 strings w/o initializing
+            string greeting = "Hello World from ";
+            string name = "Marilyn Green";
+           
+            //Output using a single Console.WriteLine()statement and the variables
+            Console.WriteLine(greeting + name);
+            Console.ReadKey();
+
+        }
+    }
+}
